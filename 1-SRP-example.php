dd8c344cd2c6ed5527bf4c2e9f9166154ee5f752
Luís Pedro Piúma <94121671+PedroPiuma@users.noreply.github.com>
2023-04-14 11:17:26 -0300

    Create 1-SRP-example.php

---
+++ 1-SRP-example.php
@@ -0,0 +1,42 @@
+<?php
+
+class Order
+{
+    public function calculateTotalSum()
+    {/*...*/
+    }
+    public function getItems()
+    {/*...*/
+    }
+    public function getItemCount()
+    {/*...*/
+    }
+    public function addItem($item)
+    {/*...*/
+    }
+    public function deleteItem($item)
+    {/*...*/
+    }
+
+    public function printOrder()
+    {/*...*/
+    }
+    public function showOrder()
+    {/*...*/
+    }
+
+    public function load()
+    {/*...*/
+    }
+    public function save()
+    {/*...*/
+    }
+    public function update()
+    {/*...*/
+    }
+    public function delete()
+    {/*...*/
+    }
+}
+
+// Reference: https://www.apphp.com/tutorials/index.php?page=solid-principles-in-php-examples
